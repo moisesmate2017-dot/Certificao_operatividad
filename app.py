2709bbb98039b4aec0265af3eea57f52c5fa13c4
moisesmate2017-dot <moisesmate2017@gmail.com>
2026-01-21 09:27:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -223,6 +223,7 @@ def generar_pdf():
     except Exception:
         ano_emision = fecha_dt.year
     considerar_nuevo_formato = 0 if ano_emision >= 2025 else 1
+    va_o_no_va = "(REVISADA 2025)" if ano_emision >= 2025 else ""
 
     # === generar PDF (manteniendo tu layout original) ===
     pdf = FPDF()
@@ -258,7 +259,7 @@ def generar_pdf():
     fecha_inspeccion_formato = fecha_dt.strftime("%d/%m/%Y")
     pdf.multi_cell(0, 4, f"""Estimado(a),
 
-Sirva la presente para saludarlo(a) cordialmente e informarle que SOLGAS S.A. con fecha {fecha_inspeccion_formato} ha realizado los trabajos de Mantenimiento Preventivo Anual en la instalación de la zona del tanque de GLP y las redes de media presión en la dirección {direccion}, en cumplimiento de la Norma Técnica Peruana NTP 321.123 (REVISADA 2025) de instalaciones de consumidores directos (Capítulo 5.2.2) y de acuerdo con los estándares de seguridad y calidad de la empresa.""")
+Sirva la presente para saludarlo(a) cordialmente e informarle que SOLGAS S.A. con fecha {fecha_inspeccion_formato} ha realizado los trabajos de Mantenimiento Preventivo Anual en la instalación de la zona del tanque de GLP y las redes de media presión en la dirección {direccion}, en cumplimiento de la Norma Técnica Peruana NTP 321.123 {va_o_no_va} de instalaciones de consumidores directos (Capítulo 5.2.2) y de acuerdo con los estándares de seguridad y calidad de la empresa.""")
     pdf.ln(3)
     pdf.multi_cell(0, 4, texto_tanques)
 
